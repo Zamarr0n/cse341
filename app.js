d515b65244e4b4230f245c52a8b7d18214810683
Zamarr0n <emilio.zamarrons@gmail.com>
2024-06-21 22:32:07 -0600

    W02 project: contacts part 2

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const mongodb = require('./data/database.js')
+const bodyParser = require('body-parser')
 const app = express();
 
 const Port = process.env.Port || 8080;
 
-
+app.use(bodyParser.json());
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    next();
+});
 app.use("/", require("./routes"));
-
 
 mongodb.main((err) => {
     if(err){
--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,13 @@
 const mongo = require('mongodb');
 const Objectid = require('mongodb').ObjectId;
 
-const getAll = async(req,res) =>{
-    const password =  encodeURIComponent(process.env.MONGO_URL);
-    const URL = `mongodb+srv://emiliozamarrons:${password}@cluster1.lgljyqa.mongodb.net/project1`;
-    const client = new mongo.MongoClient(URL);
+const password =  encodeURIComponent(process.env.MONGO_URL);
+const URL = `mongodb+srv://emiliozamarrons:${password}@cluster1.lgljyqa.mongodb.net/project1`;
+
+const client = new mongo.MongoClient(URL);
+
+const getAll = async(req,res) =>{ 
+    //#swagger.tags-['Users'] 
     const result = await client.db('project1').collection('users').find();
     result.toArray().then((users) =>{
         res.setHeader('Content-Type', 'application/json');
@@ -14,9 +17,7 @@ const getAll = async(req,res) =>{
 
 
 const getSingle = async(req,res) =>{
-    const password =  encodeURIComponent(process.env.MONGO_URL);
-    const URL = `mongodb+srv://emiliozamarrons:${password}@cluster1.lgljyqa.mongodb.net/project1`;
-    const client = new mongo.MongoClient(URL);
+    //#swagger.tags-['Users']
     const userId = new Objectid(req.params.id);
     const result = await client.db('project1').collection('users').find({_id: userId});
     result.toArray().then((users) => {
@@ -25,6 +26,59 @@ const getSingle = async(req,res) =>{
     });
 };
 
+const createUser = async(req, res) =>{
+    //#swagger.tags-['Users']
+    const user ={
+        firstName : req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+        favColor: req.body.favColor,
+        birthday: req.body.birthday
+    };
+
+    const response = await client.db('project1').collection('users').insertOne(user);
+    if(response.acknowledged){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || `Some error ocurred while creating the user.`);
+    }
+}
+
+const updateUser = async(req,res) => {
+    //#swagger.tags-['Users']
+    const userId = new Objectid(req.params.id);
+    const user ={
+        username: req.body.username,
+        email: req.body.email,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
+    };
+    const response = await client.db('project1').collection('users').replaceOne({_id: userId}, user);
+    if(response.modifiedCount > 0){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || `Some error ocurred while updating the user.`);
+    }
+}
+
+const deleteUser = async(req,res) => {
+    //#swagger.tags-['Users']
+    const userId = new Objectid(req.params.id);
+    const response = await client.db('project1').collection('users').deleteOne({_id: userId});
+    if(response.deleteCount > 0){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || `Some error ocurred while deleting the user.`);
+    }
+}
+
+
 module.exports = {
-    getAll, getSingle
+    getAll, 
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
+
 }
